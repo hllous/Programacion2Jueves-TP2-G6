ea9a8babbba40cbe685e923fe2f9a24fcfcb1c0e
Guido <guidomarolda05@gmail.com>
2025-06-03 18:41:18 -0300

    Creacion de la interface IArbol, falta implementar metodos en Arbol

--- src/models/Arbol.java
+++ src/models/Arbol.java
@@ -5,5 +5,36 @@ import interfaces.INodo;
 
 public class Arbol<T> implements IArbol<T> {
 
+    private INodo<T> raiz;
+
+    @Override
+    public void insertar(T dato) {
+
+    }
+
+    @Override
+    public void eliminar(T dato) {
+
+    }
+
+    @Override
+    public INodo buscar(T dato) {
+        return null;
+    }
+
+    @Override
+    public void preOrden() {
+
+    }
+
+    @Override
+    public void inOrden() {
+
+    }
+
+    @Override
+    public void postOrden() {
+
+    }
 
 }
