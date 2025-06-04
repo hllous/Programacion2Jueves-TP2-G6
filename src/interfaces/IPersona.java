e67a5937a03c7d88e476e5520cd3956f794c77b8
FacuIria <facuiria03@gmail.com>
2025-06-04 08:13:54 -0300

    Hice la clase Persona y su interfaz

--- src/interfaces/IPersona.java
+++ src/interfaces/IPersona.java
@@ -2,5 +2,17 @@ package interfaces;
 
 public interface IPersona {
 
+    public String getNombre();
+    public void setNombre(String nombre);
+
+    public int getDni();
+    public void setDni(int dni);
+
+    public int getEdad();
+    public void setEdad(int edad);
+    
+    @Override
+    public String toString();
+    
     
 }
--- src/models/Persona.java
+++ src/models/Persona.java
@@ -1,4 +1,34 @@
 package models;
 
-public class Persona {
+import interfaces.IPersona;
+
+public class Persona implements IPersona{
+    private String nombre;
+    private int dni;
+    private int edad;
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public int getDni() {
+        return dni;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    @Override
+    public String toString() {
+        return "Persona [nombre=" + nombre + ", dni=" + dni + ", edad=" + edad + "]";
+    }
+
+    
 }
